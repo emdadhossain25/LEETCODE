4895bd27255980122706bed9144e132964bbbdc3
emdad <emdad@ergo-ventures.com>
2023-02-23 23:50:59 +0600

    find all prime

--- findallprime.cpp
+++ findallprime.cpp
@@ -2,23 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    int a,div;
-    cin>>a;
-    while(a>2){
-        div=2;
-        while(div<a){
-            if(a%div==0){
-                --a;
-            }else{
-                ++div;
-            }
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+int a;
+int div;
+cin>>a;
+while (a>1)
+{
+    div=2;
+    while (div<a)
+    {
+        if(a%div==0){
+            --a;
+        }else{
+            ++div;
         }
-        cout<<a<<endl;
-        --a;
     }
+    cout<<a<<endl;
+    
+    --a;
+}
 
     return 0;
 }
