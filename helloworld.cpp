8567faa87ddc8bf44c250d9e8cb7a4ed55214a42
emdad <emdad@ergo-ventures.com>
2023-02-24 11:42:31 +0600

    restarting the cou rse from scratch setup vscode correctly for cpp input.txt and output.txt

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,11 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    cout<<"hello world2"<<endl;
+    int amount1;
+    int amount2;
+    cin >> amount1;
+    cin >> amount2;
+    int sum = amount1 + amount2;
+    cout << sum << endl;
     return 0;
 }
