8dd6d33a8f9b896bab4413a6290a6056acf303f0
emdad <emdad@ergo-ventures.com>
2023-02-25 20:50:51 +0600

    added rectanglePattern with hollow

--- chapter 4 pattern/rectanglePattern.cpp
+++ chapter 4 pattern/rectanglePattern.cpp
@@ -23,13 +23,43 @@ void simpleRectanglePattern()
     }
 }
 
+void hollowRectanglePattern()
+{
+
+    // ****
+    // *  *
+    // *  *
+    // *  *
+    // ****
+    // rows 5, column 4
+
+    int row, column;
+    cin >> row >> column;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= column; j++)
+        {
+
+            if (j == 1 || i == 1 || j == column || i == row)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    simpleRectanglePattern();
-
+    // simpleRectanglePattern();
+    hollowRectanglePattern();
     return 0;
 }
