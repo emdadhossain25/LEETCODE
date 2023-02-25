6abb6b5236aaf851268830645f98ff9b7d12d3e5
emdad <emdad@ergo-ventures.com>
2023-02-25 16:06:24 +0600

    prepostincrementdecrement.cpp value of pre and post increment tested

--- prepostincrementdecrement.cpp
+++ prepostincrementdecrement.cpp
@@ -2,19 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void prePostIncrement(){
+    int i = 1;
+    int j = 2;
+    int k;
+        //1 //2  //1  //2   //3   //4
+    k = i + j + i++ + j++ + ++i + ++j;
+    cout << k << endl;
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int i = 1;
-    int j = 2;
-    int k;
-           // 1   //3
-    // i = i++ + ++i;
-        //1 //2  //1  //2   //3   //4
-    k = i + j + i++ + j++ + ++i + ++j;
-    cout << k << endl;
+    prePostIncrement();
     return 0;
 }
