785d443008d865dc11fb91ad422a080421a7e526
emdad <emdad@ergo-ventures.com>
2023-02-25 17:37:44 +0600

    ternary operator

--- ternary.cpp
+++ ternary.cpp
@@ -2,15 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int a=10;
-    int b=5;
-    int c;
-    c=a>b?a-b:b-a;
-    cout<<c<<endl;
+    int a, b, c;
+    cin>>a;
+    cin>>b;
+    c = a > b ? a - b : b - a;
+    cout << c << endl;
     return 0;
 }
