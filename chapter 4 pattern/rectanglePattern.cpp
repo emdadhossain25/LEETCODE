835138f0d3a62d892dd9ba319b198b09d1145d99
emdad <emdad@ergo-ventures.com>
2023-03-01 23:31:09 +0600

    zigzag pattern

--- chapter 4 pattern/rectanglePattern.cpp
+++ chapter 4 pattern/rectanglePattern.cpp
@@ -373,7 +373,7 @@ void starPattern()
         {
             cout << " ";
         }
-        //here 2*row-1 ensures odd stars
+        // here 2*row-1 ensures odd stars
         for (int col = 1; col <= 2 * row - 1; col++)
         {
             cout << "*";
@@ -387,10 +387,38 @@ void starPattern()
         {
             cout << " ";
         }
-        //here 2*row-1 ensures odd stars
+        // here 2*row-1 ensures odd stars
         for (int col = 1; col <= 2 * row - 1; col++)
         {
             cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void zigzagPattern()
+{
+
+    //        *      *
+    //      *   *  *  *
+    //     *     *     *
+    // n =9
+    int n;
+    cin >> n;
+    // rows fixed to 3
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (((i + j) % 4 == 0)||(i==2 && j%4==0))
+            {
+                cout << "*";
+            }
+           
+            else
+            {
+                cout << " ";
+            }
         }
         cout << endl;
     }
@@ -413,6 +441,7 @@ int main()
     // rhombusPattern();
     // numberPattern();
     // pallindromicPattern();
-    starPattern();
+    // starPattern();
+    zigzagPattern();
     return 0;
 }
