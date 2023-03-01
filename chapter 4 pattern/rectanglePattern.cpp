0c48ae2312de2c47eafeaa24c92a509a6e3fcd98
emdad <emdad@ergo-ventures.com>
2023-03-01 13:22:22 +0600

    rectanglePattern

--- chapter 4 pattern/rectanglePattern.cpp
+++ chapter 4 pattern/rectanglePattern.cpp
@@ -247,27 +247,14 @@ void _01Pattern()
     {
         for (int j = 1; j <= i; j++)
         {
-            if (i % 2 == 0)
+            if ((i + j )% 2 == 0)
             {
-                if (j % 2 == 0)
-                {
-                    cout << "1 ";
-                }
-                else
-                {
-                    cout << "0 ";
-                }
+                cout << "1 ";
             }
             else
             {
-                if (j % 2 == 0)
-                {
-                    cout << "0 ";
-                }
-                else
-                {
-                    cout << "1 ";
-                }
+
+                cout << "0 ";
             }
         }
         cout << endl;
