b1b363311b1c4563e3331125a66acf1eefe9832e
emdad <emdad@ergo-ventures.com>
2023-03-02 22:42:21 +0600

    reverse a number

--- chapter 5 binary number/decimaltobinary.cpp
+++ chapter 5 binary number/decimaltobinary.cpp
@@ -10,7 +10,7 @@ void checkPrime()
     // sqrt n will cover all number
     for (int i = 2; i < sqrt(n); i++)
     {
-        cout<<sqrt(n)<<endl;
+        cout << sqrt(n) << endl;
         if (n % i == 0)
         {
             cout << "Not prime" << endl;
@@ -23,13 +23,29 @@ void checkPrime()
         cout << "Prime" << endl;
     }
 }
+void reverseNumber()
+{
+    // 1234 input
+    // 4321 output
+    int n;
+    cin >> n;
+    int reverse = 0;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        reverse = reverse * 10 + lastDigit;
+        n /= 10;
+    }
+    cout << reverse << endl;
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    checkPrime();
+    // checkPrime();
+    reverseNumber();
 
     return 0;
 }
