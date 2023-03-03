0fe1a0dc5b639b09f82d9e5df270476b0294658d
emdad <emdad@ergo-ventures.com>
2023-03-03 20:56:55 +0600

    functionsFactorialForTwoNumbers

---
+++ chapter 6 function/factorialForTwoNumbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void factorialSum(int n)
+{
+
+    int factorial = 0;
+    while (n)
+    {
+        factorial += n;
+        n--;
+    }
+    cout << factorial << endl;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n1, n2;
+    cin >> n1 >> n2;
+    factorialSum(n1);
+    factorialSum(n2);
+    return 0;
+}
