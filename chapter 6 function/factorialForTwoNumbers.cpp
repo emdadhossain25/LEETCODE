a5df18d0f5a2893997d971bece415bb4ab48418d
emdad <emdad@ergo-ventures.com>
2023-03-04 00:19:08 +0600

    factorialTwoNumbers.cpp

--- chapter 6 function/factorialForTwoNumbers.cpp
+++ chapter 6 function/factorialForTwoNumbers.cpp
@@ -13,6 +13,31 @@ void factorialSum(int n)
     }
     cout << factorial << endl;
 }
+void primeNumbersBetweenTwoinputs(int startNumber, int endNumber)
+{
+
+    while (endNumber >= startNumber)
+    {
+        bool flag = 0; // to check full iteration
+
+        for (int i = 2; i < endNumber; i++)
+        {
+            if (endNumber % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        // if full iteration from 2 to endNumber-1 done
+        // without dividing then its a prime
+        if (flag == 0)
+        {
+            cout << endNumber << endl;
+        }
+
+        endNumber--;
+    }
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -21,7 +46,8 @@ int main()
 #endif
     int n1, n2;
     cin >> n1 >> n2;
-    factorialSum(n1);
-    factorialSum(n2);
+    // factorialSum(n1);
+    // factorialSum(n2);
+    primeNumbersBetweenTwoinputs(min(n1, n2), max(n1, n2));
     return 0;
 }
