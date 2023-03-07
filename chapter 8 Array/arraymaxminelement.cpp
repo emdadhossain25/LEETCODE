46d6307e7961c197f1c60b072afa7dd74ddf41fb
emdad <emdad@ergo-ventures.com>
2023-03-07 22:23:05 +0600

    array finding max min elements

--- chapter 8 Array/arraymaxminelement.cpp
+++ chapter 8 Array/arraymaxminelement.cpp
@@ -4,23 +4,20 @@ using namespace std;
 
 int32_t main()
 {
-    int n,
-        maxNumber = INT_MIN,
-        minNumber = INT_MAX;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    for (int j = 0; j < n; j++)
-    {
-
-        maxNumber= max(arr[j],maxNumber);
-        minNumber=min(arr[j],minNumber);
-    }
-    cout << maxNumber << " " << minNumber << endl;
+   int n, maxNumber=INT_MIN, minNumber=INT_MAX;
+   cin>>n;
+   int arr[n];
+   for (int i = 0; i < n; i++)
+   {
+    cin>>arr[i];
+   }
+   for (int j = 0; j < n; j++)
+   {
+    maxNumber=max(arr[j],maxNumber);
+    minNumber=min(arr[j],minNumber);
+   }
+   cout<<maxNumber<<" "<<minNumber;
+   
 
     return 0;
 }
