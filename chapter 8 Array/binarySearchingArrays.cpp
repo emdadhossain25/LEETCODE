b9b9b717ea80509c21ba075899e870e85532af52
emdad <emdad@ergo-ventures.com>
2023-03-08 11:42:50 +0600

    binary search

--- chapter 8 Array/binarySearchingArrays.cpp
+++ chapter 8 Array/binarySearchingArrays.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+// time complexity: n/2, n/4~ n/2powern~ log2n or logn
 int binarySearch(int arr[], int findElement, int n)
 {
 
     int startIndex = 0;
     int endIndex = n - 1;
-
     while (startIndex <= endIndex)
     {
-        int midIndex = (endIndex + startIndex) / 2;
-        if (findElement > arr[midIndex])
-        {
-            startIndex = midIndex + 1;
-        }
-        else if (findElement < arr[midIndex])
-        {
-            endIndex = midIndex - 1;
-        }
-        else
+        int midIndex = (startIndex + endIndex) / 2;
+        if (arr[midIndex] == findElement)
         {
             return midIndex;
         }
+        else if (findElement > arr[midIndex])
+        {
+            startIndex = midIndex + 1;
+        }
+        else
+        {
+            endIndex = midIndex - 1;
+        }
     }
-
     return -1;
 }
 
