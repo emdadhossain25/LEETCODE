c589dc179103ab7473f303cb74ddbc2b1e35e0bb
emdad <emdad@ergo-ventures.com>
2023-03-11 23:44:22 +0600

    all prime numbers

--- allPrimeNumbers.cpp
+++ allPrimeNumbers.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+// #endif
     int n;
     cin >> n;
     while (n > 1)
