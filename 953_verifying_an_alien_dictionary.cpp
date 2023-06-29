6dda534c0e9fa2e1948ce4c9ca3ae88443b5b471
emdad <emdad@ergo-ventures.com>
2023-06-29 13:11:17 +0600

    953 verifying an alien directory

---
+++ 953_verifying_an_alien_dictionary.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef long long ll;
+#define ff first
+#define ss second
+#define REP(i, a, b) for (int i = a; i <= b; ++i)
+
+bool isAlienSorted(vector<string> &words, string order)
+{
+  bool ans=false;
+
+  return ans;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  vector<string> words = {"word", "world", "row"};
+  string order = "worldabcefghijkmnpqstuvxyz";
+
+  return 0;
+}
